7e242c1423fe392e22872beabcf9aa30fe14d9db
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-25 06:10:17 +0300

    Add stream from FirebaseAuth

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,16 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:imenniroutes/imenniroutes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Login',
-        ),
-      ),
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SignInScreen(
+            providers: [],
+          );
+        }
+        return const Imenniroutes();
+      },
     );
   }
 }
