14b1a20642034be3832c20a7a3e5198e5f36c24f
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-25 06:08:08 +0300

    Move MyApp class from main.dart to a seperate file and rename it to Imenniroutes

---
+++ lib/imenniroutes.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:imenniroutes/gemini.dart';
+import 'package:imenniroutes/login_page.dart';
+import 'package:imenniroutes/map.dart';
+
+class Imenniroutes extends StatelessWidget {
+  const Imenniroutes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            // Use Column for vertical arrangement
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Center content vertically
+            children: [
+              const Text(
+                'demos',
+                style: TextStyle(fontSize: 32),
+              ),
+              const SizedBox(height: 20), // Add space between text and button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const map()),
+                  );
+                },
+                child: const Text('map'),
+              ),
+              const SizedBox(height: 20), // Add space between text and button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const gemini()),
+                  );
+                },
+                child: const Text('gemini'),
+              ),
+              const SizedBox(height: 20), // Add space between text and button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+                child: const Text('login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
