5637f11cb7bd32e39b88c2627326f4edafbdeb04
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-25 06:09:09 +0300

    Initialize Firebase and remove MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:imenniroutes/gemini.dart';
+import 'package:imenniroutes/firebase_options.dart';
 import 'package:imenniroutes/login_page.dart';
-import 'package:imenniroutes/map.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MaterialApp(
-      home: MyApp(),
+      home: LoginPage(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            // Use Column for vertical arrangement
-            mainAxisAlignment:
-                MainAxisAlignment.center, // Center content vertically
-            children: [
-              const Text(
-                'demos',
-                style: TextStyle(fontSize: 32),
-              ),
-              const SizedBox(height: 20), // Add space between text and button
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const map()),
-                  );
-                },
-                child: const Text('map'),
-              ),
-              const SizedBox(height: 20), // Add space between text and button
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const gemini()),
-                  );
-                },
-                child: const Text('gemini'),
-              ),
-              const SizedBox(height: 20), // Add space between text and button
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: const Text('login'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
