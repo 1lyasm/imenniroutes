e20746feacf88da0df86f89285b768ee85235344
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-25 06:33:48 +0300

    Add email to service providers list

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:imenniroutes/imenniroutes.dart';
@@ -12,8 +12,10 @@ class LoginPage extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return const SignInScreen(
-            providers: [],
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
           );
         }
         return const Imenniroutes();
