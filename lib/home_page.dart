a0264a7b979ebd5f2816e034c29d9784b86951b6
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-26 04:03:38 +0300

    Add profile page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:imenniroutes/gemini.dart';
 import 'package:imenniroutes/map.dart';
@@ -9,32 +10,74 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<ProfileScreen>(
+                    builder: (context) => ProfileScreen(
+                      appBar: AppBar(
+                        title: const Text('User Profile'),
+                      ),
+                      actions: [
+                        SignedOutAction((context) {
+                          Navigator.of(context).pop();
+                        })
+                      ],
+                      children: [
+                        const Divider(),
+                        Padding(
+                          padding: const EdgeInsets.all(2),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.asset(
+                                'assets/flutterfire_300x.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+          automaticallyImplyLeading: false,
+        ),
         body: Center(
           child: Column(
             // Use Column for vertical arrangement
-            mainAxisAlignment:
-                MainAxisAlignment.center, // Center content vertically
+            mainAxisAlignment: MainAxisAlignment
+                .center, // Center content vertically
             children: [
               const Text(
                 'demos',
                 style: TextStyle(fontSize: 32),
               ),
-              const SizedBox(height: 20), // Add space between text and button
+              const SizedBox(
+                  height:
+                      20), // Add space between text and button
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const map()),
+                    MaterialPageRoute(
+                        builder: (context) => const map()),
                   );
                 },
                 child: const Text('map'),
               ),
-              const SizedBox(height: 20), // Add space between text and button
+              const SizedBox(
+                  height:
+                      20), // Add space between text and button
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const gemini()),
+                    MaterialPageRoute(
+                        builder: (context) => const gemini()),
                   );
                 },
                 child: const Text('gemini'),
