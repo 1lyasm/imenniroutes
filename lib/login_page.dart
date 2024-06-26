57311f3046e2733c4dbead213e1504752f746db3
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-06-26 04:03:03 +0300

    Add widgets to login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:imenniroutes/home_page.dart';
@@ -16,6 +17,45 @@ class LoginPage extends StatelessWidget {
             providers: [
               EmailAuthProvider(),
             ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child:
+                      Image.asset('assets/flutterfire_300x.png'),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text(
+                        'Welcome to FlutterFire, please sign in!')
+                    : const Text(
+                        'Welcome to Flutterfire, please sign up!'),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            sideBuilder: (context, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('flutterfire_300x.png'),
+                ),
+              );
+            },
           );
         }
         return const HomePage();
